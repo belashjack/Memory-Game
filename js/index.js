fc849ab97db4d853e420fe9fc5d74cd0cfb48088
belashjack <belashjack1@gmail.com>
2018-05-02 16:18:57 +0300

    Memory-Game:/fix: make consistent with AirBnb styleguide

--- js/index.js
+++ js/index.js
@@ -8,34 +8,36 @@ class Game {
         this.flippedCardsAmount = 0;
         this.frontSides = [];
         this.timeouts = [];
-    }
+    };
     getPlayerInfo() {
-        this.playerInfo.firstName = form.elements['firstName'].value;
-        this.playerInfo.lastName = form.elements['lastName'].value;
-        this.playerInfo.email = form.elements['email'].value;
-    }
+        this.playerInfo.firstName = form.elements.firstName.value;
+        this.playerInfo.lastName = form.elements.lastName.value;
+        this.playerInfo.email = form.elements.email.value;
+    };
     getNumberOfCards() {
         this.numberOfCards = Number(difficultiesContainer.querySelector('.active-difficulty').dataset.amount);
-    }
+    };
     getBackSide() {
         this.backSide = getComputedStyle(patternContainer.querySelector('.active-pattern')).backgroundImage;
-    }
+    };
     getFrontSides() {
         this.frontSides = frontSides.slice(0, this.numberOfCards / 2).concat(frontSides.slice(0, this.numberOfCards / 2));
-    }
+    };
     createCards() {
+        // auxiliary function to get random integer in range
+        const getRandomInt = function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+
         // auxiliary function to get random background image of the front side
         const getRandomFrontSide = function getRandomFrontSide(frontSides, numberOfCards) {
-            let index = getRandomInt(0, frontSides.length - 1);
+            const index = getRandomInt(0, frontSides.length - 1);
             const frontSide = frontSides[index];
-            frontSides.splice(index, 1);
-            return frontSide;
-        }
 
-        // auxiliary function to get random integer in range
-        function getRandomInt(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+            frontSides.splice(index, 1);
+            
+            return frontSide;
+        };
 
         for (let i = 0; i < this.numberOfCards; i++) {
             const card = new Card(i, this.backSide, getRandomFrontSide(this.frontSides, this.numberOfCards));
@@ -46,36 +48,37 @@ class Game {
         this.cards.forEach((card) => {
             field.appendChild(card);
         });
-    }
+    };
     addGameListeners() {
         field.addEventListener('click', Card.turnCard);
-    }
+    };
     getFlippedCards() {
         return [...field.querySelectorAll('.flipped')];
-    }
+    };
     compareCards(flippedCards) {
         if (getComputedStyle(flippedCards[0].children[0]).backgroundImage === getComputedStyle(flippedCards[1].children[0]).backgroundImage) {
             return true;
         }
+
         return false;
-    }
+    };
     showResult() {
         field.innerHTML = '';
         field.style.display = 'none';
         result.style.display = 'flex';
-        resultValue.innerHTML = timerDOMElem.innerHTML + ' sec.';
-    }
+        resultValue.innerHTML = `${timerDOMElem.innerHTML} sec.`;
+    };
     showGame() {
         homepage.style.display = 'none';
         gamepage.style.display = 'flex';
         field.style.display = 'flex';
         window.scrollTo(0, 0);
-    }
+    };
     hideGame() {
         homepage.style.display = 'flex';
         gamepage.style.display = 'none';
         window.scrollTo(0, 0);
-    }
+    };
     stopGame() {
         timer.stop();
         field.innerHTML = '';
@@ -84,7 +87,7 @@ class Game {
         this.timeouts.forEach((timeout) => {
             clearTimeout(timeout);
         });
-    }
+    };
 }
 
 class Card {
@@ -92,7 +95,7 @@ class Card {
         this.id = id;
         this.backSide = backSide;
         this.frontSide = frontSide;
-    }
+    };
     getDOMElem() {
         const card = document.createElement('section');
         card.classList.add('card');
@@ -115,7 +118,7 @@ class Card {
         card.appendChild(cardInner);
 
         return card;
-    }
+    };
     static turnCard(event) {
         // cancel if not clicked on card
         if (!event.target.classList.contains('back')) {
@@ -137,7 +140,8 @@ class Card {
 
         // compare two flipped cards and remove them or flip them back
         if (game.flippedCardsAmount === 2) {
-            let flippedCards = game.getFlippedCards();
+            const flippedCards = game.getFlippedCards();
+
             if (game.compareCards(flippedCards)) {
                 flippedCards.forEach((elem) => {
                     const timeout1 = setTimeout(() => {
@@ -159,6 +163,7 @@ class Card {
                             // save result into localStorage
                             storeResult();
                         }
+
                         game.flippedCardsAmount = 0;
                     }, 1900);
                     game.timeouts.push(timeout1, timeout2);
@@ -181,24 +186,25 @@ class Card {
 class Timer {
     constructor() {
         this.startDate = 0;
-    }
+    };
     start() {
         timerDOMElem.innerHTML = String(this.startDate);
         this.startDate = new Date();
         this.timerId = setInterval(() => {
             timerDOMElem.innerHTML = Math.round((Date.now() - this.startDate.getTime()) / 1000);
         }, 1000);
-    }
+    };
     stop() {
         game.playerInfo.stopTime = new Date().getTime();
         clearInterval(this.timerId);
-    }
+    };
 }
 
 const startGame = function startGame() {
     const checkFormValidity = function checkFormValidity() {
         const elements = [...form.elements];
         let hasMistakes = false;
+
         elements.forEach((elem) => {
             if (!elem.checkValidity()) {
                 hasMistakes = true;
@@ -208,11 +214,13 @@ const startGame = function startGame() {
                 elem.classList.remove('invalid-field');
             }
         });
+
         if (document.querySelector('.invalid-field') !== null) {
             document.querySelector('.invalid-field').focus();
         }
+
         return !hasMistakes;
-    }
+    };
 
     // simple form validation
     if (!checkFormValidity()) {
@@ -245,7 +253,7 @@ const startGame = function startGame() {
 
     // start timer
     timer.start();
-}
+};
 const restartGame = function restartGame() {
     // stop game (stop timer, clear field and result, cancel existing timeout actions)
     game.stopGame();
@@ -257,8 +265,8 @@ const restartGame = function restartGame() {
     timer = new Timer();
 
     // start new game with new objects
-    startGame(game, timer);
-}
+    startGame();
+};
 const stopGame = function stopGame() {
     // stop game (stop timer, clear field and result, cancel existing timeout actions)
     game.stopGame();
@@ -271,7 +279,7 @@ const stopGame = function stopGame() {
 
     // create new timer object instead of previous one
     timer = new Timer();
-}
+};
 const storeResult = function storeResult() {
     const archive = [];
     const keys = Object.keys(localStorage);
@@ -293,13 +301,15 @@ const storeResult = function storeResult() {
             if (Number(JSON.parse(a).time) > Number(JSON.parse(b).time)) {
                 return 1;
             }
+
             return Number(JSON.parse(a).stopTime) - Number(JSON.parse(b).stopTime);
         });
+
         for (let i = 1; i <= archive.length && i <= 10; i++) {
             localStorage.setItem(i, archive[i - 1]);
         }
     }
-}
+};
 const showScoreboard = function showScoreboard() {
     homepage.style.display = 'none';
     scoreboard.style.display = 'flex';
@@ -333,27 +343,33 @@ const showScoreboard = function showScoreboard() {
         const firstName = document.createElement('td');
         const lastName = document.createElement('td');
         const time = document.createElement('td');
+
         place.innerHTML = keys[i];
         firstName.innerHTML = JSON.parse(archive[i]).firstName;
         lastName.innerHTML = JSON.parse(archive[i]).lastName;
-        time.innerHTML = JSON.parse(archive[i]).time + ' sec.';
+        time.innerHTML = `${JSON.parse(archive[i]).time} sec.`;
+
         row.appendChild(place);
         row.appendChild(firstName);
         row.appendChild(lastName);
         row.appendChild(time);
         tbody.appendChild(row);
     }
-}
+};
 const hideScoreboard = function hideScoreboard() {
     homepage.style.display = 'flex';
     scoreboard.style.display = 'none';
     scoreboardTable.tBodies[0].innerHTML = '';
-}
+};
 
 const addListeners = function addListeners() {
     const changeActivePattern = function changeActivePattern() {
-        if (event.target.classList.contains('back-side-pattern') && !event.target.classList.contains('active-pattern')) {
+        if (
+            event.target.classList.contains('back-side-pattern')
+            && !event.target.classList.contains('active-pattern')
+        ) {
             const children = [...event.target.parentNode.children];
+
             for (let i = 0; i < children.length; i++) {
                 if (children[i].classList.contains('active-pattern')) {
                     children[i].classList.remove('active-pattern');
@@ -361,12 +377,13 @@ const addListeners = function addListeners() {
                 }
             }
             event.target.classList.add('active-pattern');
-        };
+        }
     };
     const changeActiveDifficulty = function changeActiveDifficulty() {
         if (event.target.closest('.difficulty') !== null) {
             const target = event.target.closest('.difficulty');
             const children = [...target.parentNode.children];
+
             for (let i = 0; i < children.length; i++) {
                 if (children[i].classList.contains('active-difficulty')) {
                     children[i].classList.remove('active-difficulty');
@@ -374,33 +391,46 @@ const addListeners = function addListeners() {
                 }
             }
             target.classList.add('active-difficulty');
+        }
+    };
+    function forceBlur(event) {
+        const handler = function handler() {
+            event.target.blur();
+            document.body.removeEventListener('mouseup', handler);
         };
+        document.body.addEventListener('mouseup', handler);
     };
     const addKeyboardControl = function addKeyboardControl(event) {
         if (event.target.classList.contains('back-side-pattern') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             changeActivePattern();
         }
+
         if (event.target.classList.contains('difficulty') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             changeActiveDifficulty();
         }
+
         if (event.target.classList.contains('play-button') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             startGame();
         }
+
         if (event.target.classList.contains('scoreboard-button') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             showScoreboard();
         }
+
         if (event.target.classList.contains('scoreboard-homepage-button') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             hideScoreboard();
         }
+
         if (event.target.classList.contains('game-homepage-button') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             stopGame();
         }
+
         if (event.target.classList.contains('play-again-button') && (event.keyCode === keycodes.ENTER || event.keyCode === keycodes.SPACE)) {
             event.preventDefault();
             restartGame();
@@ -409,6 +439,9 @@ const addListeners = function addListeners() {
 
     patternContainer.addEventListener('mousedown', changeActivePattern);
     difficultiesContainer.addEventListener('mousedown', changeActiveDifficulty);
+    buttons.forEach((button) => {
+        button.addEventListener('mousedown', forceBlur);
+    })
 
     // add keyboard control
     document.body.addEventListener('keydown', addKeyboardControl);
@@ -427,15 +460,16 @@ const addListeners = function addListeners() {
 
     // stop game when clicked on HOMEPAGE button
     gameHomepageButton.addEventListener('click', stopGame);
-}
+};
 
 // get elements on the homepage
 const homepage = document.querySelector('.homepage');
-const form = document.forms['info'];
+const form = document.form.info;
 const patternContainer = document.querySelector('.back-sides-container');
 const difficultiesContainer = document.querySelector('.difficulties-container');
 const playButton = document.querySelector('.play-button');
 const scoreboardButton = document.querySelector('.scoreboard-button');
+const buttons = document.querySelectorAll('.button');
 
 // get elements on the scoreboard
 const scoreboard = document.querySelector('.scoreboard');
